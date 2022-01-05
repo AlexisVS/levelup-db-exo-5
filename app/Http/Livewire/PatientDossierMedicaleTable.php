0cbef800dae8a30d82a169e68c6e4a99718436ef
AlexisVS <63750707+AlexisVS@users.noreply.github.com>
2022-01-05 23:13:16 +0100

    zer

--- app/Http/Livewire/PatientDossierMedicaleTable.php
+++ app/Http/Livewire/PatientDossierMedicaleTable.php
@@ -9,28 +9,24 @@ use App\Models\Dossier_medicale;
 
 class PatientDossierMedicaleTable extends DataTableComponent
 {
-
+    public bool $showSearch = false;
     public function columns(): array
     {
         return [
             Column::make('#', 'id')
-            ->sortable()
-            ->searchable(),
-        Column::make('N° registre national', 'numero_registre_national')
-            ->sortable()
-            ->searchable(),
-        Column::make('Nom de maladie', 'maladies.nom')
-            ->sortable()
-            ->searchable(),
-        Column::make('Status dossier médicale', 'dossier_medicale_statuses.status')
-            ->sortable()
-            ->searchable(),
-        Column::make('Identifiant de consultation', 'consultation_id')
-            ->sortable()
-            ->searchable(),
-        Column::make('Diagnostic', 'diagnostic')
-            ->sortable()
-            ->searchable(),
+                ->sortable()
+                ->searchable(),
+            Column::make('N° registre national', 'numero_registre_national')
+                ->sortable()
+                ->searchable(),
+            Column::make('Nom de maladie', 'maladies.nom'),
+            Column::make('Status dossier médicale', 'dossier_medicale_statuses.status'),
+            Column::make('Identifiant de consultation', 'consultation_id')
+                ->sortable()
+                ->searchable(),
+            Column::make('Diagnostic', 'diagnostic')
+                ->sortable()
+                ->searchable(),
         ];
     }
 
